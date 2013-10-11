4ab85cb730cc9f78dbef2b537801cc4efbf26638
bmros <bl.rossier@bluewin.ch>
2013-10-11 15:48:25 +0200

    after hours...

--- app/controllers/#sessions_controller.rb
+++ app/controllers/#sessions_controller.rb
@@ -12,22 +12,12 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-<<<<<<< HEAD
-	  redirect_to user
-	  #redirect_to user_cars_path(user)
-	  session[:current_user_id] = user.id
-=======
 	  #redirect_to user
 	  redirect_to user_cars_path(user)
->>>>>>> 1863f9aa536fe5a8fe8c5d021e67d1b99b51e50a
     end
   end
   
   def destroy
-<<<<<<< HEAD
-  	session[:current_user_id] = nil
-=======
->>>>>>> 1863f9aa536fe5a8fe8c5d021e67d1b99b51e50a
     sign_out
     redirect_to root_path
   end  
